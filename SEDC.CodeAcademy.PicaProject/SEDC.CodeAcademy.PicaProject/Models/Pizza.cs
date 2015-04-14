263492ae7f51e62eca321465e1438f1a7f42b059
joconolimits <joconolimits@gmail.com>
2015-04-14 18:34:07 +0200

    Pizza Model created

---
+++ SEDC.CodeAcademy.PicaProject/SEDC.CodeAcademy.PicaProject/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEDC.CodeAcademy.PicaProject.Models
+{
+    public class Pizza
+    {
+        public int ID { get; set; }
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public List<string> Ingredients { get; set; }
+
+       
+    }
+}
