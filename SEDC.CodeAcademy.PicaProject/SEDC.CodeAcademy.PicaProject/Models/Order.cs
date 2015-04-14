bdb557fb4a98defc399d55a5eeb2de0d78ddb67e
joconolimits <joconolimits@gmail.com>
2015-04-14 18:42:01 +0200

    new commit

---
+++ SEDC.CodeAcademy.PicaProject/SEDC.CodeAcademy.PicaProject/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SEDC.CodeAcademy.PicaProject.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public int PizzaID { get; set; }
+        public string Address { get; set; }
+
+    }
+}
